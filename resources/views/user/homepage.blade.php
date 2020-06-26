2f86d03d4c50db6e4d194b8c26ae60cfead9d91f
Mocha-Hub <arifin.education123@gmail.com>
2020-06-26 15:40:53 +0700

    Layout User

--- resources/views/user/homepage.blade.php
+++ resources/views/user/homepage.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('/style_user/style.css')}}">
+    <link rel="stylesheet" href="{{asset('/style_user/sidebar.css')}}">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
     integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
     crossorigin=""/>
@@ -13,97 +14,38 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="area"><div id="mapid"></div></div>
-    <nav class="main-menu">
-            <ul>
-                <li>
-                    <a href="http://justinfarrow.com">
-                        <i class="fa fa-home fa-2x"></i>
-                        <span class="nav-text">
-                            Dashboard
-                        </span>
-                    </a>
-                  
-                </li>
-                <li class="has-subnav">
-                    <a href="#">
-                        <i class="fa fa-laptop fa-2x"></i>
-                        <span class="nav-text">
-                            Stars Components
-                        </span>
-                    </a>
-                    
-                </li>
-                <li class="has-subnav">
-                    <a href="#">
-                       <i class="fa fa-list fa-2x"></i>
-                        <span class="nav-text">
-                            Forms
-                        </span>
-                    </a>
-                    
-                </li>
-                <li class="has-subnav">
-                    <a href="#">
-                       <i class="fa fa-folder-open fa-2x"></i>
-                        <span class="nav-text">
-                            Pages
-                        </span>
-                    </a>
-                   
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa fa-bar-chart-o fa-2x"></i>
-                        <span class="nav-text">
-                            Graphs and Statistics
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa fa-font fa-2x"></i>
-                        <span class="nav-text">
-                           Quotes
-                        </span>
-                    </a>
-                </li>
-                <li>
-                   <a href="#">
-                       <i class="fa fa-table fa-2x"></i>
-                        <span class="nav-text">
-                            Tables
-                        </span>
-                    </a>
-                </li>
-                <li>
-                   <a href="#">
-                        <i class="fa fa-map-marker fa-2x"></i>
-                        <span class="nav-text">
-                            Maps
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                       <i class="fa fa-info fa-2x"></i>
-                        <span class="nav-text">
-                            Documentation
-                        </span>
-                    </a>
-                </li>
+    <div id="mapid"></div>
+    <nav role="navigation">
+        <div id="menuToggle">
+            <input type="checkbox"/>
+            <span></span>
+            <span></span>
+            <span></span>
+            <ul id="menu">
+                <a href="#">
+                    <li class="between">
+                        <img style="width: 40px; height: 40px; margin-top: 10px; margin-left: 10px" src="/gambar/marker/logo.png">
+                        <p >PetaJalan Prambanan</p>
+                    </li>
+                </a>
+                <a href="#"><li class="between">
+                    <i class="fa fa-location-arrow"></i>
+                    <p>Rute</p>
+                </li></a>
+                <a href="#"><li class="between">
+                    <i class="fa fa-bullhorn"></i>
+                    <p>Suara Warga</p>
+                </li></a>
+                <a href="#"><li class="between">
+                    <i class="fa fa-info"></i>
+                    <p>Pusat Informasi</p>
+                </li></a>
+                <a href="/login"><li class="between">
+                    <i class="fa fa-power-off"></i>
+                    <p>Login</p>
+                </li></a>
             </ul>
-
-            <ul class="logout">
-                <li>
-                   <a href="/logout">
-                         <i class="fa fa-power-off fa-2x"></i>
-                        <span class="nav-text">
-                            Logout
-                        </span>
-                    </a>
-                </li>  
-            </ul>
+        </div>
     </nav>
     <script src="{{asset('js_admin/bundle.js')}}"></script>
     <script src="{{asset('js_admin/show_map.js')}}"></script>
